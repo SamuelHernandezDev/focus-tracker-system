47bc8858ffc45964a4a2684873610afb518f6039
Sam <l21210671@tectijuana.edu.mx>
2026-05-27 14:04:24 -0700

    fix: update next.confit.tsh

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,8 +5,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: false,
 
-  allowedDevOrigins: ['192.168.100.3'],
-
   images: {
     remotePatterns: [
       {
